6f95c18b112b32945ef897ce05b1b256a349b0c2
Bruno Vitorino <bruno.vitorino@tecnico.ulisboa.pt>
2026-06-03 21:54:08 +0100

    Add raw Markdown links to docs pages

--- docs/official/.vitepress/theme/index.ts
+++ docs/official/.vitepress/theme/index.ts
@@ -5,10 +5,33 @@ import DefaultTheme from 'vitepress/theme'
 import { createMermaidRenderer } from 'vitepress-mermaid-renderer'
 import './style.css'
 
+const rawContentBase = 'https://raw.githubusercontent.com/BrunoV21/FerrisGrid-CLI/main/docs/official/'
+
 export default {
   extends: DefaultTheme,
   Layout: () => {
-    const { isDark } = useData()
+    const { isDark, page } = useData()
+
+    const RawMarkdownLink = () => {
+      if (page.value.isNotFound || !page.value.relativePath) {
+        return null
+      }
+
+      const rawUrl = `${rawContentBase}${page.value.relativePath}`
+
+      return h('div', { class: 'raw-markdown-link' }, [
+        h(
+          'a',
+          {
+            href: rawUrl,
+            target: '_blank',
+            rel: 'noopener',
+            'data-raw-markdown-link': page.value.relativePath
+          },
+          'View raw Markdown'
+        )
+      ])
+    }
 
     const renderMermaid = () => {
       createMermaidRenderer({
@@ -24,6 +47,9 @@ export default {
     nextTick(renderMermaid)
     watch(() => isDark.value, renderMermaid)
 
-    return h(DefaultTheme.Layout)
+    return h(DefaultTheme.Layout, null, {
+      'doc-before': RawMarkdownLink,
+      'home-features-after': RawMarkdownLink
+    })
   }
 } satisfies Theme
